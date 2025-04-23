385313ffcf51eaa7853bc87eb6e8827c283e4e9e
ahmed123-coder <156669117+ahmed123-coder@users.noreply.github.com>
2025-04-22 18:40:44 -0700

    Update Skills.jsx

--- frontend/src/components/Skills.jsx
+++ frontend/src/components/Skills.jsx
@@ -16,7 +16,7 @@ export const Skills = () => {
   useEffect(()=> {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/service");
+        const response = await axios.get("https://ahmedkhmiri.onrender.com/api/service");
         setSkills(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
