e52c92915dbaf7f84b0b53fb0c2240893fa16cd9
ahmed123-coder <156669117+ahmed123-coder@users.noreply.github.com>
2025-05-14 02:42:02 -0700

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,10 @@ const Home = () => {
   }, []);
   return (
     <div className="App">
-<NavBar logo={`${siteContent?.logoheader}`}  siteName={siteContent?.siteName}/>
+<NavBar 
+  logo={siteContent?.logoheader} 
+  siteName={siteContent?.siteName}
+/>
 <Banner hero={siteContent?.hero} logohero={`https://mywebprofile-1.onrender.com/${siteContent?.logohero}`}/>
 <Skills />
 <Projects />
